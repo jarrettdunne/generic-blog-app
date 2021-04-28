14ba95aed9a132ff50c40b611aa8875cb6be9864
jarrett <jarrettpdunne@gmail.com>
2021-04-27 21:47:23 -0500

    update react components with templates

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function PostEdit() {
+export default function PostCreate() {
     return (
         <div>
             
---
+++ client/src/screens/UserCreate/UserCreate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserCreate() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
