15c9a162b1127319cf95f6524fb0ff0076e6abcb
jarrett <jarrettpdunne@gmail.com>
2021-04-28 17:55:58 -0500

    update postMain elements and styling

--- client/src/components/PostMain.jsx
+++ client/src/components/PostMain.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 
+import bookmarkIcon from '../assests/bookmark-icon.png'
+import commentIcon from '../assests/comment-icon.png'
+import heartIcon from '../assests/heart-icon.png'
+import shareIcon from '../assests/share-icon.png'
+
 import './styles/PostMain.css'
 
 export default class PostMain extends Component {
     render() {
         return (
             <div className="post-wrapper">
-                <div className="post-bar"></div>
+                <div className="post-bar">
+                    <div className="post-bar-like icon">
+                        <img src={heartIcon} alt="heart"/>
+                    </div>
+                    <div className="post-bar-comment icon">
+                        <img src={commentIcon} alt="comment"/>
+                    </div>
+                    <div className="post-bar-saved icon">
+                        <img src={bookmarkIcon} alt="bookmark"/>
+                    </div>
+                    <div className="post-bar-share icon">
+                        <img src={shareIcon} alt="share"/>
+                    </div>
+                </div>
                 <div className="post-contents">
-                    <div className="post-header"></div>
+                    <div className="post-header">
+                        <div className="post-header-about"></div>
+                        <div className="post-header-title"></div>
+                    </div>
                     <div className="post-content"></div>
                 </div>
             </div>
