7075d26fdf258e95bc59372b942a6219ba590d1b
jarrett <jarrettpdunne@gmail.com>
2021-04-29 01:57:50 -0500

    update create post form and styling:

--- client/src/screens/PostCreate.jsx
+++ client/src/screens/PostCreate.jsx
@@ -5,11 +5,11 @@ import './styles/PostCreate.css'
 export default function PostCreate() {
     return (
         <div className="post-create-wrapper">
-            Create a post
             <form className="post-create-form">
-                <input type="text" name="title" placeholder="title" />
-                <textarea name="content" id="" cols="30" rows="10"></textarea>
-                <input className="post-create-submit" type="submit" value="post"/>
+                <div className="user-create-form-name">Create a post</div>
+                <input className="post-create-title" required type="text" name="title" placeholder="title (required)" />
+                <textarea className="post-create-content" required name="content" placeholder="text (required)" cols="30" rows="10"></textarea>
+                <input className="post-create-submit" type="submit" value="Post"/>
             </form>
         </div>
     )
