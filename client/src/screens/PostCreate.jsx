2bbfb59bf35920a26c36be453cad8657abd907d5
jarrett <jarrettpdunne@gmail.com>
2021-04-29 01:24:36 -0500

    add create post form elements

--- client/src/screens/PostCreate.jsx
+++ client/src/screens/PostCreate.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import './styles/PostCreate.css'
+
 export default function PostCreate() {
     return (
-        <div>
-            
+        <div className="post-create-wrapper">
+            Create a post
+            <form className="post-create-form">
+                <input type="text" name="title" placeholder="title" />
+                <textarea name="content" id="" cols="30" rows="10"></textarea>
+                <input className="post-create-submit" type="submit" value="post"/>
+            </form>
         </div>
     )
 }
