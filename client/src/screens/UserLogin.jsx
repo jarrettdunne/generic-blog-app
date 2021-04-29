f2a468c7a1a71094d97bda8a22b45e6f4a4ce368
jarrett <jarrettpdunne@gmail.com>
2021-04-28 23:13:33 -0500

    add user login elements

--- client/src/screens/UserLogin.jsx
+++ client/src/screens/UserLogin.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function UserLogin() {
     return (
-        <div>
-            
+        <div className="user-create-wrapper">
+            <form className="user-create-form">
+                <div className="user-create-form-name">Login</div> 
+                <input className='user-create-input' type="text" placeholder='email' />
+                <input className='user-create-input' type="text" placeholder='password' />
+                <input className='user-create-submit' type="submit" value="submit"/>
+                <div>Don’t have one? <Link to='/register'>Create account</Link></div>
+                
+            </form>
         </div>
     )
 }
