53ca3a59d323ffdc814c9f57b7e2bce3246e8678
jarrett <jarrettpdunne@gmail.com>
2021-04-29 03:32:15 -0500

    add comment section below post

--- client/src/screens/PostDetail.jsx
+++ client/src/screens/PostDetail.jsx
@@ -9,27 +9,33 @@ import './styles/PostDetail.css'
 
 export default function PostDetail(props) {
     return (
-        <div className="post-wrapper post-detail-wrapper">
-            <div className="post-bar">
-                <div className="post-bar-like">
-                    <img className="icon" src={heartIcon} alt="heart" />
+        <div>
+            <div className="post-wrapper post-detail-wrapper">
+                <div className="post-bar">
+                    <div className="post-bar-like">
+                        <img className="icon" src={heartIcon} alt="heart" />
+                    </div>
+                    <div className="post-bar-comment">
+                        <img className="icon" src={commentIcon} alt="comment" />
+                    </div>
+                    <div className="post-bar-saved">
+                        <img className="icon" src={bookmarkIcon} alt="bookmark" />
+                    </div>
+                    <div className="post-bar-share">
+                        <img className="icon" src={shareIcon} alt="share" />
+                    </div>
                 </div>
-                <div className="post-bar-comment">
-                    <img className="icon" src={commentIcon} alt="comment" />
-                </div>
-                <div className="post-bar-saved">
-                    <img className="icon" src={bookmarkIcon} alt="bookmark" />
-                </div>
-                <div className="post-bar-share">
-                    <img className="icon" src={shareIcon} alt="share" />
+                <div className="post-contents">
+                    <div className="post-header">
+                        <div className="post-header-about"></div>
+                        <div className="post-header-title"></div>
+                    </div>
+                    <div className="post-content"></div>
                 </div>
             </div>
-            <div className="post-contents">
-                <div className="post-header">
-                    <div className="post-header-about"></div>
-                    <div className="post-header-title"></div>
-                </div>
-                <div className="post-content"></div>
+            <div className="comments-wrapper">
+                Comments
+                <div className=""></div>
             </div>
         </div>
     )
