57b4490c4b786191b3894017ef5062a5f40fc24b
jarrett <jarrettpdunne@gmail.com>
2021-04-29 09:18:38 -0500

    render post data to DOM

--- client/src/screens/PostDetail.jsx
+++ client/src/screens/PostDetail.jsx
@@ -8,29 +8,31 @@ import shareIcon from '../assests/share_black_48dp.svg'
 import './styles/PostDetail.css'
 
 export default function PostDetail(props) {
+    const { post } = props
     return (
         <div>
-            <div className="post-wrapper post-detail-wrapper">
+            <div className="post-wrapper home-post">
                 <div className="post-bar">
                     <div className="post-bar-like">
-                        <img className="icon" src={heartIcon} alt="heart" />
-                    </div>
-                    <div className="post-bar-comment">
-                        <img className="icon" src={commentIcon} alt="comment" />
+                        <img className="icon" src={heartIcon} alt="heart"/>
                     </div>
                     <div className="post-bar-saved">
-                        <img className="icon" src={bookmarkIcon} alt="bookmark" />
+                        <img className="icon" src={bookmarkIcon} alt="bookmark"/>
+                    </div>
+                    <hr/>
+                    <div className="post-bar-comment">
+                        <img className="icon" src={commentIcon} alt="comment"/>
                     </div>
                     <div className="post-bar-share">
-                        <img className="icon" src={shareIcon} alt="share" />
+                        <img className="icon" src={shareIcon} alt="share"/>
                     </div>
                 </div>
                 <div className="post-contents">
                     <div className="post-header">
-                        <div className="post-header-about"></div>
-                        <div className="post-header-title"></div>
+                        <div className="post-header-about">{post.created_at}</div>
+                        <div className="post-header-title">{post.title}</div>
                     </div>
-                    <div className="post-content"></div>
+                    <div className="post-content">{post.content}</div>
                 </div>
             </div>
             <div className="comments-wrapper">
