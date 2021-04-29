b0273373ad3ab089b1d69ddf3ace5f6f03b6606b
jarrett <jarrettpdunne@gmail.com>
2021-04-28 23:28:46 -0500

    add state to user login form inputs

--- client/src/screens/UserLogin.jsx
+++ client/src/screens/UserLogin.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function UserLogin() {
+export default function UserLogin(props) {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+    const { username, password } = formData;
+    const { handleLogin } = props;
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        handleLogin(formData)
+    }
+
     return (
         <div className="user-create-wrapper">
-            <form className="user-create-form">
-                <div className="user-create-form-name">Login</div> 
-                <input className='user-create-input' type="text" placeholder='email' />
-                <input className='user-create-input' type="text" placeholder='password' />
-                <input className='user-create-submit' type="submit" value="submit"/>
+            <form className="user-create-form" onSubmit={handleSubmit}>
+                <div className="user-create-form-name">Login</div>
+                <input className='user-create-input' type="text" name='username' placeholder='username' value={username} onChange={handleChange} />
+                <input className='user-create-input' type="password" name='password' placeholder='password' value={password} onChange={handleChange} />
+                <input className='user-create-submit' type="submit" value="submit" />
                 <div>Don’t have one? <Link to='/register'>Create account</Link></div>
-                
             </form>
         </div>
     )
