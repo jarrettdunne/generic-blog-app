d1fbb71737d3744018cf8261f20c0c77ac21ed03
jarrett <jarrettpdunne@gmail.com>
2021-04-30 12:25:22 -0500

    remove format column from posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_171221) do
+ActiveRecord::Schema.define(version: 2021_04_30_172227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2021_04_29_171221) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.text "format"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
