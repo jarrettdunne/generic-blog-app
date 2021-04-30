e377f767ce137b9b35b954a758626d5bbd944736
jarrett <jarrettpdunne@gmail.com>
2021-04-30 16:28:02 -0500

    change mapped component to PostSmall

--- client/src/screens/UserHome.jsx
+++ client/src/screens/UserHome.jsx
@@ -3,13 +3,15 @@ import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 
 import PostMain from '../components/PostMain'
+import PostSmall from '../components/PostSmall'
 import { getUserPosts } from '../services/posts'
 
 import './styles/UserHome.css'
 
-export default function UserHome() {
+export default function UserHome(props) {
     const [userPosts, setUserPosts] = useState([])
     const params = useParams()
+    const { handleDelete } = props
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -23,7 +25,7 @@ export default function UserHome() {
         return userPosts.map((v, i) => (
             <div className="home-posts-container-item" key={i}>
                 <Link to={`/posts/${v.id}`}>
-                    <PostMain post={v} />
+                    <PostSmall handleDelete={handleDelete} post={v} />
                 </Link>
             </div>
         ))
