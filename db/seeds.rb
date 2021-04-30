9963da25ac3c52223b6720b120f2e39b86f65330
jarrett <jarrettpdunne@gmail.com>
2021-04-30 14:47:48 -0500

    update seed to create two users with associated posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,27 +10,20 @@ Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
-user_list = [
-    {
-        "username": "user1",
-        "email": "user1@email.com",
-        "password": "123456"
-    }
-]
-
 lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-@user = User.create!(username: 'user1', email: 'user1@email.com', password: '123456')
+@user1 = User.create!(username: 'user1', email: 'user1@email.com', password: '123456')
+@user2 = User.create!(username: 'user2', email: 'user2@email.com', password: '654321')
 puts "#{User.count} users created."
 
-@post1 = Post.create!(title: 'This is a blog 1', content: lorem, format: 'none', user: @user)
-Post.create!(title: 'This is a blog 2', content: lorem, format: 'none', user: @user)
-Post.create!(title: 'This is a blog 3', content: lorem, format: 'none', user: @user)
-Post.create!(title: 'This is a blog 4', content: lorem, format: 'none', user: @user)
-Post.create!(title: 'This is a blog 5', content: lorem, format: 'none', user: @user)
-Post.create!(title: 'This is a blog 6', content: lorem, format: 'none', user: @user)
-Post.create!(title: 'This is a blog 7', content: lorem, format: 'none', user: @user)
+@post1 = Post.create!(title: 'This is a blog 1', content: lorem, user: @user1)
+Post.create!(title: 'This is a blog 2', content: lorem, user: @user1)
+Post.create!(title: 'This is a blog 3', content: lorem, user: @user1)
+Post.create!(title: 'This is a blog 4', content: lorem, user: @user1)
+Post.create!(title: 'This is a blog 5', content: lorem, user: @user2)
+Post.create!(title: 'This is a blog 6', content: lorem, user: @user2)
+Post.create!(title: 'This is a blog 7', content: lorem, user: @user2)
 puts "#{Post.count} posts created."
 
-Comment.create!(title: 'My comment', content: 'Lorem ipsum', format: '{}', user: @user, post: @post1)
+Comment.create!(title: 'My comment', content: 'Lorem ipsum', format: '{}', user: @user1, post: @post1)
 puts "#{Comment.count} comments created."
