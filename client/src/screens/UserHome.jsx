0c6861d23a572ffc88d1ecd2d0c5abab45dfba23
jarrett <jarrettpdunne@gmail.com>
2021-04-30 14:56:57 -0500

    add useEffect and useState for getting user posts

--- client/src/screens/UserHome.jsx
+++ client/src/screens/UserHome.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
+
+import PostMain from '../components/PostMain'
+import { getUserPosts } from '../services/posts'
+
+import './styles/UserHome.css'
 
 export default function UserHome() {
+    const [userPosts, setUserPosts] = useState([])
+    const params = useParams()
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const postData = await getUserPosts(params.id)
+            setUserPosts(postData)
+        }
+        fetchPosts()
+    }, [params.id])
+
+    const posts = () => {
+        return userPosts.map((v, i) => (
+            <div className="home-posts-container-item" key={i}>
+                <Link to={`/posts/${v.id}`}>
+                    <PostMain post={v} />
+                </Link>
+            </div>
+        ))
+    }
     return (
-        <div>
-            
+        <div className="home-posts-container">
+            {posts()}
         </div>
     )
 }
