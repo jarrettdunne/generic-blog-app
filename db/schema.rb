53fd9f7427b1730033c9a298efd9f030de9fc701
jarrett <jarrettpdunne@gmail.com>
2021-04-30 17:49:54 -0500

    update schema to reflect migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_172227) do
+ActiveRecord::Schema.define(version: 2021_04_30_222656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_172227) do
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.text "format"
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
