c87af32c7f3ceaf125fbb131e8964fb3daa509e4
jarrett <jarrettpdunne@gmail.com>
2021-05-01 10:00:38 -0500

    add elements and render comment data

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
+import './styles/Comment.css'
+
 export default class Comment extends Component {
+    constructor(props) {
+        super(props)
+        this.comment = props.comment
+        console.log(this.comment)
+    }
     render() {
         return (
-            <div>
-                
+            <div className="comment-wrapper">
+                <div className="comment-bar"></div>
+                <div className="comment-contents">
+                    <div className="comment-header">
+                        <div className="comment-username">{this.comment.user_id}</div>
+                        <div className="comment-time">{this.comment.created_at}</div>
+                    </div>
+                    <div className="comment-content">{this.comment.content}</div>
+                </div>
             </div>
         )
     }
