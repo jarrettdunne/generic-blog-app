30fd07b0bffc2ce03ae3d3fdd15e56d2d1e81ff4
jarrett <jarrettpdunne@gmail.com>
2021-05-01 14:22:22 -0500

    add handls for input change, form submit and comment creation

--- client/src/components/CommentCreate.jsx
+++ client/src/components/CommentCreate.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useHistory } from 'react-router'
 
+import { postComment } from '../services/comments'
 import UserOptions from './UserOptions'
 
 export default function CommentCreate(props) {
+    const params = useParams()
+    const history = useHistory()
+    const { id } = params
     const { currentUser } = props
+    const [content, setContent] = useState(
+        {
+            content: ''
+        }
+    )
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setContent(prevState => ({
+            ...prevState,   
+            [name]: value
+        }))
+    }
+
+    const handleCreate = async (id, content) => {
+        console.log(id, content)
+        const data = await postComment(id, content);
+        setContent(prevState => [...prevState, data])
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        handleCreate(id, content)
+        history.go(0)
+    }
+
     return (
-        <form className="create-comment-wrapper">
-            <textarea className="create-comment-textarea" placeholder="What are your thought?" name="" id="" cols="0" rows="0"></textarea>
+        <form className="create-comment-wrapper" onSubmit={handleSubmit}>
+            <input 
+                className="create-comment-textarea" 
+                type="text" 
+                required 
+                placeholder="How does this make you feel?" 
+                name="content"
+                onChange={handleChange}>
+            </input>
             <div className="comment-create-options">
                 {
                     currentUser
-                    ?
-                    <input className="comment-create-submit" type="submit" value="Comment" />
-                    :
-                    <UserOptions />
+                        ?
+                        <input className="comment-create-submit" type="submit" value="Comment" />
+                        :
+                        <UserOptions />
                 }
             </div>
         </form>
