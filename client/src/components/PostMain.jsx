731c6ebd1b86724c37a94e2a79c30d83fb86c134
jarrett <jarrettpdunne@gmail.com>
2021-05-01 21:32:38 -0500

    wrap icon img in buttons add state interface interactions

--- client/src/components/PostMain.jsx
+++ client/src/components/PostMain.jsx
@@ -1,36 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import bookmarkIcon from '../assests/bookmark_border_black_48dp.svg'
-import commentIcon from '../assests/mode_comment_black_48dp.svg'
-import heartIcon from '../assests/favorite_border_black_48dp.svg'
+import unsavedIcon from '../assests/bookmark_border_black_48dp.svg'
+import savedIcon from '../assests/bookmark_black_48dp.svg'
+import commentIcon from '../assests/comment_black_48dp.svg'
+import unfavIcon from '../assests/favorite_border_black_48dp.svg'
+import favIcon from '../assests/favorite_black_48dp.svg'
 import shareIcon from '../assests/share_black_48dp.svg'
 
 import './styles/PostMain.css'
 
 export default function PostMain(props) {
+    const [fav, setFav] = useState(true)
+    const [icon, setIcon] = useState(unfavIcon)
+    const [likes, setLikes] = useState(0)
+    const [save, setSave] = useState(true)
+    const [saveIcon, setSaveIcon] = useState(unsavedIcon)
     const { post } = props
+
+    const handleLike = (e) => {
+        e.preventDefault()
+        setFav(prev => !prev)
+        if (fav) {
+            setIcon(favIcon)
+            setLikes(prev => prev += 1)
+        } else {
+            setIcon(unfavIcon)
+            setLikes(prev => prev -= 1)
+        }
+    }
+
+    const handleSave = (e) => {
+        e.preventDefault()
+        setSave(prev => !prev)
+        if (save) {
+            setSaveIcon(savedIcon)
+        } else {
+            setSaveIcon(unsavedIcon)
+        }
+    }
+
+    const handleShare = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="post-wrapper">
             <div className="post-bar">
-                <div className="post-bar-like">
-                    <img className="icon" src={heartIcon} alt="heart" />
+                <div className="post-bar-like post-bar-item">
+                    <button onClick={handleLike}>
+                        <img className="icon clickable-icon" src={icon} alt="heart" />
+                        <div className="num-likes">{likes}</div>
+                    </button>
                 </div>
-                <div className="post-bar-saved">
-                    <img className="icon" src={bookmarkIcon} alt="bookmark" />
+                <div className="post-bar-saved post-bar-item">
+                    <button onClick={handleSave}>
+                        <img className="icon clickable-icon" src={saveIcon} alt="bookmark" />
+                    </button>
+                </div>
+                <div className="post-bar-share post-bar-item">
+                    <button onClick={handleShare}>
+                        <img className="icon clickable-icon" src={shareIcon} alt="share" />
+                    </button>
                 </div>
                 <hr/>
-                <div className="post-bar-comment">
+                <div className="post-bar-comment post-bar-item">
                     <img className="icon" src={commentIcon} alt="comment" />
-                    {
-                        post.comments
-                        ?
-                        post.comments.length
-                        :
-                        0
-                    }
-                </div>
-                <div className="post-bar-share">
-                    <img className="icon" src={shareIcon} alt="share" />
+                    <div className="num-comments">
+                        {
+                            post.comments
+                            ?
+                            post.comments.length
+                            :
+                            0
+                        }
+                    </div>
                 </div>
             </div>
             <div className="post-contents">
