16c85528ae15d5d6cb8df21532e319384e970d72
jarrett <jarrettpdunne@gmail.com>
2021-05-01 22:21:07 -0500

    add general routes for likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
-
+  
   get '/user/:id/posts', to: 'posts#get_user_posts'
-
+  
   post '/posts/:id/comments', to: 'comments#create'
-
+  
+  resources :likes
   # resources :comments
   resources :posts
   resources :users
