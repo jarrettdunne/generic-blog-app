766f00c8f7dd77da2a9ef9ab86613d0920a992da
jarrett <jarrettpdunne@gmail.com>
2021-05-03 01:18:17 -0500

    add trending controller and update user posts controller for comments and likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,20 @@
 class PostsController < ApplicationController
-  before_action :authorize_request, only: [:create, :update, :destroy]
-  before_action :set_post, only: :show
+  before_action :authorize_request, only: [:create, :update, :destroy, :like]
+  before_action :set_post, only: [:show, :like]
   before_action :set_user_post, only: [:update, :destroy]
 
   # GET /posts
   def index
-    @posts = Post.includes(:comments)
+    @posts = Post.all
+    # @posts = Post.includes(:comments)
     
-    render json: @posts.as_json(include: { comments: { only: [:user_id, :content, :created_at]} })
+    render json: @posts.as_json(include: { comments: { only: [:user_id, :content, :created_at] }, likes: {only: [:user_id, :post_id]} })
   end
 
   # GET /posts/1
   def show
-    render json: @post.as_json(include: { comments: { only: [:user_id, :content, :created_at]} })
+    @post.likes.count
+    render json: @post.as_json(include: { comments: { only: [:user_id, :content, :created_at] }, likes: {only: [:user_id, :post_id]} })
   end
 
   # POST /posts
@@ -45,7 +47,33 @@ class PostsController < ApplicationController
   def get_user_posts
     @user_posts = User.find(params[:id]).posts
 
-    render json: @user_posts
+    render json: @user_posts.as_json(include: { comments: { only: [:user_id, :content, :created_at] }, likes: {only: [:user_id, :post_id]} })
+  end
+
+  def like
+    @like = Like.create(user_id: @current_user.id, post_id: @post.id)
+
+    render json: @like, status: :created
+  end
+
+  def trending
+    @posts = Post.all
+    @BoW = {}
+    @posts.each do |post|
+      words = post.content.split().concat post.title.split()
+      words.each do |word|
+        word = word.downcase
+        if @BoW[word]
+          @BoW[word] += 1
+        else
+          @BoW[word] = 1
+        end
+      end
+    end
+
+    temp = @BoW.sort_by { |key, val| -val }
+
+    render json: temp
   end
 
   private
