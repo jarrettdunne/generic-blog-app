2f621bc782aeb11a12cb743e65da492d6bee5549
jarrett <jarrettpdunne@gmail.com>
2021-05-03 02:37:50 -0500

    update user_id to username and convert time

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import { timeSince } from '../services/funcs'
+
 import './styles/Comment.css'
 
 export default class Comment extends Component {
@@ -13,8 +15,14 @@ export default class Comment extends Component {
                 <div className="comment-bar"></div>
                 <div className="comment-contents">
                     <div className="comment-header">
-                        <div className="comment-username">{this.comment.user_id}</div>
-                        <div className="comment-time">{this.comment.created_at}</div>
+                        <div className="comment-username">
+                            {this.comment.user.username}
+                            &nbsp;
+                            <div className="comment-time">
+                                {timeSince(this.comment.created_at)}
+                            </div>
+                        </div>
+                        
                     </div>
                     <div className="comment-content">{this.comment.content}</div>
                 </div>
