a472b31f163aebe7cea60d88a8d985d01ffc147b
jarrett <jarrettpdunne@gmail.com>
2021-05-03 01:22:52 -0500

    update seed with nested user, posts, comments and likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,33 +11,52 @@ Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
-lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+Faker::UniqueGenerator.clear
 
-@user1 = User.create!(username: 'user1', email: 'user1@email.com', password: '123456')
-@user2 = User.create!(username: 'user2', email: 'user2@email.com', password: '654321')
+@user = User.create!(username: 'user', email: 'user@email.com', password: '123456')
+
+(1..10).each do |i|
+    user = User.create!(
+        username: Faker::Internet.unique.username, 
+        email: Faker::Internet.unique.email, 
+        password: Faker::Internet.password(min_length: 8))
+    rand = Random.rand(5..20)
+    (1..rand).each do |j|
+        post = Post.create!(
+            title: Faker::Hipster.unique.sentence, 
+            content: Faker::Hipster.unique.paragraph, 
+            user: user)
+    end
+end
+
+        
+u0 = User.first.id
+un = User.last.id
+p0 = Post.first.id
+pn = Post.last.id
+
+(p0..pn).each do |i|
+    post = Post.find(i)
+    rand = Random.rand(3..15)
+
+    (1..rand).each do |j|
+        user = User.find(Random.rand(u0..un))
+        Comment.create!(
+            content: Faker::Hipster.sentence(word_count: Random.rand(10)), 
+            user: user,
+            post: post)
+    end
+
+    (u0..un).each do |j|
+        if Random.rand(0..1) == 1
+            Like.create!(
+                user: User.find(j), 
+                post: post)
+        end
+    end
+end
+        
 puts "#{User.count} users created."
-
-@post1 = Post.create!(title: 'This is a blog 1', content: lorem, user: @user1)
-@post2 = Post.create!(title: 'This is a blog 2', content: lorem, user: @user1)
-Post.create!(title: 'This is a blog 3', content: lorem, user: @user1)
-Post.create!(title: 'This is a blog 4', content: lorem, user: @user1)
-Post.create!(title: 'This is a blog 5', content: lorem, user: @user2)
-Post.create!(title: 'This is a blog 6', content: lorem, user: @user2)
-Post.create!(title: 'This is a blog 7', content: lorem, user: @user2)
 puts "#{Post.count} posts created."
-
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post1)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post2)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post1)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post2)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post1)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post2)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post1)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post2)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post1)
-Comment.create!(content: 'Lorem ipsum', user: @user1, post: @post2)
 puts "#{Comment.count} comments created."
-
-Like.create!(user: @user1, post: @post1)
-Like.create!(user: @user2, post: @post1)
 puts "#{Like.count} likes created."
